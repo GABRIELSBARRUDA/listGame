f679d15bfcff509621caad1dc073d797a1eded13
GABRIEL ARRUDA <obielsbueno@gmail.com>
2023-11-18 14:15:30 -0300

    Game Seed
    Signed-off-by: GABRIEL ARRUDA <obielsbueno@gmail.com>

--- src/main/java/com/gabrielArruda/listGame/entities/Game.java
+++ src/main/java/com/gabrielArruda/listGame/entities/Game.java
@@ -20,10 +20,15 @@ public class Game {
 
     @Column(name = "game_year") /* Customizando nome no banco */
     private Integer year;
+    private Double score;
     private String genre;
-    private String platform;
+    private String platforms;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT") /* Definindo tipo da coluna no banco */
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT") /* Definindo tipo da coluna no banco */
     private String longDescription;
 
 
@@ -31,14 +36,15 @@ public class Game {
 
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform, 
+    public Game(Long id, String title, Integer year, Double score, String genre, String platforms, 
         String imgUrl, String shortDescription, String longDescription){
         
         this.id = id;
         this.title = title;
         this.year = year;
+        this.score = score;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -69,6 +75,14 @@ public class Game {
         this.year = year;
     }
 
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
     public String getGenre() {
         return genre;
     }
@@ -77,12 +91,12 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatform(String platforms) {
+        this.platforms = platforms;
     }
 
     public String getImgUrl() {
